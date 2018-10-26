efa83cbd243d67e87af6d9c1a52a1f9909e3c253
Roddy Vitali <roddicks@gmail.com>
2018-10-26 12:59:06 -0300

    Upload Final

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg')
 const pool = new Pool({
   host: 'localhost',
   port: 5432,
-  user: 'postgres',
+  user: 'roy',
   password: '123456',
   database: 'postgres'
 });
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/data', async (req, res) => {
+router.post('/posts', async (req, res) => {
   const text = 'INSERT INTO posts(name, description) VALUES($1, $2) RETURNING *'
   const values = [req.body.name, req.body.description]
   try {
@@ -26,7 +26,7 @@ router.post('/data', async (req, res) => {
   }
 });
 
-router.delete('/data/:postId', async (req, res) => {
+router.delete('/posts/:postId', async (req, res) => {
   const text = 'DELETE FROM posts WHERE id=($1) RETURNING *'
   const values = [req.params.postId]
   try {
@@ -37,7 +37,7 @@ router.delete('/data/:postId', async (req, res) => {
   }
 });
 
-router.get('/data', async (req, res) => {
+router.get('/posts', async (req, res) => {
   try {
     const response = await pool.query('SELECT * FROM posts')
     res.json( { success: true, response: response.rows })
